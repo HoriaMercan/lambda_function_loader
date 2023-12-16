1b8584a3fb8c808f30ce37b61a851a74891e5caf
Horia Mercan <horia.mercan@gmail.com>
2023-12-16 11:12:54 +0200

    merge sa vorbeasca cat de cat

--- src/ipc.c
+++ src/ipc.c
@@ -9,32 +9,47 @@
 
 #include "ipc.h"
 
+#include "utils.h"
+
 
 int create_socket(void)
 {
 	/* TODO: Implement create_socket(). */
-	return -1;
+	int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+	return fd;
+
 }
 
 int connect_socket(int fd)
 {
-	/* TODO: Implement connect_socket(). */
-	return -1;
+/* TODO: Implement connect_socket(). */
+	struct sockaddr_un server_addr;
+	server_addr.sun_family = AF_UNIX;
+	strcpy(server_addr.sun_path, SOCKET_NAME);
+	size_t server_addr_size = sizeof(server_addr);
+
+	printf("try connect\n");
+	int rc = connect(fd, (struct  sockaddr *)&server_addr, server_addr_size);
+	return rc;
 }
 
 ssize_t send_socket(int fd, const char *buf, size_t len)
 {
 	/* TODO: Implement send_socket(). */
-	return -1;
+	int rc = send(fd, buf, len, 0);
+	return rc;
 }
 
 ssize_t recv_socket(int fd, char *buf, size_t len)
 {
 	/* TODO: Implement recv_socket(). */
-	return -1;
+	int rc = recv(fd, buf, len, 0);
+	return rc;
 }
 
 void close_socket(int fd)
 {
 	/* TODO: Implement close_socket(). */
+	close(fd);
 }
--- src/server.c
+++ src/server.c
@@ -7,10 +7,14 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #include "ipc.h"
 #include "server.h"
+
+#include "utils.h"
 
 #ifndef OUTPUT_TEMPLATE
 #define OUTPUT_TEMPLATE "../checker/output/out-XXXXXX"
@@ -83,11 +87,37 @@ static int parse_command(const char *buf, char *name, char *func, char *params)
 int main(void)
 {
 	/* TODO: Implement server connection. */
+	int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	struct sockaddr_un server_addr;
+	struct sockaddr_un client_addr;
+
+	server_addr.sun_family = AF_UNIX;
+	strcpy(server_addr.sun_path, SOCKET_NAME);
+	size_t server_addr_size = sizeof(server_addr);
+
+	int bindret = bind(fd, (struct sockaddr *)&server_addr, server_addr_size);
+	DIE(bindret < 0, "bind");
+	int listenret = listen(fd, MAX_CLIENTS);
+	DIE(listenret < 0, "listen");
+	char buf[1024];
 	int ret;
 	struct lib lib;
 
 	while (1) {
 		/* TODO - get message from client */
+
+		int clen = sizeof(client_addr);
+		int client_socket = accept(fd, (struct sockaddr *) &client_addr, &clen);
+		if (client_socket == -1)
+			continue;
+//		printf("%d\n", client_socket);
+		int size = 0;
+		while (size == 0) {
+			size = recv(client_socket, buf, 1024, 0);
+		}
+		strcpy(buf, "ana are mere");
+
+		write(client_socket, buf, 1024);
 		/* TODO - parse message with parse_command and populate lib */
 		/* TODO - handle request from client */
 		ret = lib_run(&lib);
